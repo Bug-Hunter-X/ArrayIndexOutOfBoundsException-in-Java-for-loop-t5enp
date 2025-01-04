13d628c839db6f9defaebaadb8aa3e4ad7d41ef7
BugHunter-X <askzenai@gmail.com>
2025-01-04 22:46:04 +0100

    Add bugSolution.java file

---
+++ bugSolution.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i * 2; 
+        }
+        //To access the last element 
+        System.out.println(arr[arr.length -1]);
+    }
+}
